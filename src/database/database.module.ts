594d1173623d2b242fe0c90a2a513c4e4f9eada9
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-09 20:12:15 +0100

    delete separate db module and move conn to app module

--- src/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import dataSource from 'db/data-source';
-
-@Module({
-  imports: [TypeOrmModule.forRootAsync(dataSource)],
-  exports: [TypeOrmModule],
-})
-export class DatabaseModule {}
