4af6162fa6382a79701a90c40c724e218fef750f
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-11 08:36:24 +0100

    register entities in their modules

--- src/studentclass/studentclass.module.ts
+++ src/studentclass/studentclass.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StudentclassService } from './studentclass.service';
 import { StudentclassController } from './studentclass.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Studentclass } from './entities/studentclass.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Studentclass])],
   controllers: [StudentclassController],
   providers: [StudentclassService],
 })
--- src/subjects/subjects.module.ts
+++ src/subjects/subjects.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SubjectsService } from './subjects.service';
 import { SubjectsController } from './subjects.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subject } from './entities/subject.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Subject])],
   controllers: [SubjectsController],
   providers: [SubjectsService],
 })
