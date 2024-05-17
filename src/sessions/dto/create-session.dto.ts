e4eb1b8d889a0fc14bf5539cf2e560f66db96f55
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-17 21:57:56 +0100

    add session dto and services methods

--- src/sessions/dto/create-session.dto.ts
+++ src/sessions/dto/create-session.dto.ts
@@ -1,1 +1,13 @@
-export class CreateSessionDto {}
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+import { IsUnique } from 'src/shared/isUniqueConstraint';
+import { SessionStatus } from 'src/types/types';
+
+export class CreateSessionDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsUnique({ tableName: 'session', column: 'name' })
+  name: string;
+
+  @IsEnum(SessionStatus)
+  status: SessionStatus;
+}
