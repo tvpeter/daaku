cbadecfb7c046c2e3e971649e20cb201c92b6b60
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-18 19:38:06 +0100

    add user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,39 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  Length,
+} from 'class-validator';
+import { Role } from 'src/shared/types';
+
+export class CreateUserDto {
+  @IsString()
+  @Length(3, 20)
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  name: string;
+
+  @IsStrongPassword()
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  sig_url: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(11, 11)
+  phone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role: Role;
+}
