19f30a1973f1784a636053ad37af16635312b4e9
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-18 19:31:29 +0100

    add isregistered constraint

---
+++ src/shared/is-registered-constraint.ts
@@ -0,0 +1,55 @@
+import { Injectable, Logger } from '@nestjs/common';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { EntityManager } from 'typeorm';
+import { IsUniqueConstraintInput } from './types';
+
+@Injectable()
+export class IsRegisteredConstraint implements ValidatorConstraintInterface {
+  constructor(private readonly entityManager: EntityManager) {}
+
+  async validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> {
+    try {
+      const { tableName, column }: IsUniqueConstraintInput =
+        validationArguments.constraints[0];
+
+      const exists = await this.entityManager
+        .getRepository(tableName)
+        .createQueryBuilder(tableName)
+        .where({ [column]: value })
+        .getExists();
+      return exists ? true : false;
+    } catch (error) {
+      Logger.log(error);
+      return false;
+    }
+  }
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    const { tableName, column }: IsUniqueConstraintInput =
+      validationArguments.constraints[0];
+    return `selected ${tableName} ${column} does not exist`;
+  }
+}
+
+export function IsRegistered(
+  options: IsUniqueConstraintInput,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'is-registered',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [options],
+      validator: IsRegisteredConstraint,
+    });
+  };
+}
