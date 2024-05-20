217c3c0cb695f3eeaa3df850a79921c4a1c2c098
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-20 15:15:53 +0100

    add create dto for result status

--- src/result-status/dto/create-result-status.dto.ts
+++ src/result-status/dto/create-result-status.dto.ts
@@ -1,1 +1,19 @@
-export class CreateResultStatusDto {}
+import { IsEnum } from 'class-validator';
+import { Session } from 'src/sessions/entities/session.entity';
+import { IsRegistered } from 'src/shared/is-registered-constraint';
+import { ResultStatusEnum, SchoolTerm } from 'src/shared/types';
+import { Studentclass } from 'src/studentclass/entities/studentclass.entity';
+
+export class CreateResultStatusDto {
+  @IsEnum(SchoolTerm)
+  term: SchoolTerm;
+
+  @IsEnum(ResultStatusEnum)
+  result_status: ResultStatusEnum;
+
+  @IsRegistered({ tableName: 'session', column: 'id' })
+  session: Session;
+
+  @IsRegistered({ tableName: 'studentclass', column: 'id' })
+  class: Studentclass;
+}
--- src/result-status/result-status.module.ts
+++ src/result-status/result-status.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResultStatusService } from './result-status.service';
 import { ResultStatusController } from './result-status.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ResultStatus } from './entities/result-status.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ResultStatus])],
   controllers: [ResultStatusController],
   providers: [ResultStatusService],
 })
