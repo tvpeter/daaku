b709787c05691a7c2843512f013213954d920e6a
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-22 13:20:26 +0100

    add check for existing score for a student

--- src/scores/scores.service.ts
+++ src/scores/scores.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateScoreDto } from './dto/create-score.dto';
 import { UpdateScoreDto } from './dto/update-score.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -13,6 +18,15 @@ export class ScoresService {
   ) {}
   async create(createScoreDto: CreateScoreDto) {
     const score = this.scoreRepository.create(createScoreDto);
+
+    const checkScoreExists = await this.scoreExists(createScoreDto);
+    console.log(JSON.stringify(checkScoreExists));
+    if (checkScoreExists) {
+      throw new HttpException(
+        'Given subject score exists for the student for selected term and session',
+        HttpStatus.PRECONDITION_FAILED,
+      );
+    }
 
     return await this.scoreRepository.save(score);
   }
@@ -39,5 +53,18 @@ export class ScoresService {
   async remove(id: number) {
     const score = await this.findOne(id);
     return this.scoreRepository.remove(score);
+  }
+
+  async scoreExists(score: CreateScoreDto) {
+    const { term, student_id, class_id, session_id, subject_id } = score;
+    return await this.scoreRepository.findOne({
+      where: {
+        term,
+        student_id,
+        class_id,
+        session_id,
+        subject_id,
+      },
+    });
   }
 }
