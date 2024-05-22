f473438926a4400444e880e64ec1ca694f16b3ea
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-22 12:42:36 +0100

    add scores dto

--- src/scores/dto/create-score.dto.ts
+++ src/scores/dto/create-score.dto.ts
@@ -1,1 +1,45 @@
-export class CreateScoreDto {}
+import { IsEnum, IsNumber, IsOptional, IsPositive } from 'class-validator';
+import { IsRegistered } from 'src/shared/is-registered-constraint';
+import { SchoolTerm } from 'src/shared/types';
+
+export class CreateScoreDto {
+  @IsEnum(SchoolTerm)
+  term: SchoolTerm;
+
+  @IsOptional()
+  @IsPositive()
+  @IsNumber()
+  test: number;
+
+  @IsOptional()
+  @IsPositive()
+  @IsNumber()
+  exam: number;
+
+  @IsOptional()
+  total: number;
+
+  @IsOptional()
+  class_avg: number;
+
+  @IsOptional()
+  lowest: number;
+
+  @IsOptional()
+  highest: number;
+
+  @IsOptional()
+  subject_position: number;
+
+  @IsRegistered({ tableName: 'students', column: 'id' })
+  student_id: number;
+
+  @IsRegistered({ tableName: 'studentclass', column: 'id' })
+  class_id: number;
+
+  @IsRegistered({ tableName: 'session', column: 'id' })
+  session_id: number;
+
+  @IsRegistered({ tableName: 'subjects', column: 'id' })
+  subject_id: number;
+}
