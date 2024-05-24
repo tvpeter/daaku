9a9da75bce715a74bb1770eb3d24996ca7b41726
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-24 17:46:06 +0100

    add result entity

--- src/results/entities/result.entity.ts
+++ src/results/entities/result.entity.ts
@@ -1,1 +1,44 @@
-export class Result {}
+import { SchoolTerm } from '@app/shared/types';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('results')
+export class Result {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'enum', enum: SchoolTerm })
+  term: SchoolTerm;
+
+  @Column({ default: 0 })
+  subjects_total: number;
+
+  @Column({
+    type: 'numeric',
+    nullable: true,
+    default: 0,
+    precision: 4,
+    scale: 2,
+  })
+  student_total: number;
+
+  @Column({
+    type: 'numeric',
+    nullable: true,
+    default: 0.0,
+    precision: 4,
+    scale: 2,
+  })
+  student_avg: number;
+
+  @Column()
+  position: number;
+
+  @Column({ type: 'text', nullable: true })
+  principal_comments: string;
+
+  @Column({ nullable: true, type: 'text' })
+  teacher_comment: string;
+
+  @Column({ default: 0, nullable: true })
+  checks: number;
+}
