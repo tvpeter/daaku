f866feb2e57e894483a0c9c89dba95933cc43046
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-25 18:33:26 +0100

    add result dto

--- src/results/dto/create-result.dto.ts
+++ src/results/dto/create-result.dto.ts
@@ -1,1 +1,23 @@
-export class CreateResultDto {}
+import { IsRegistered } from '@app/shared/is-registered-constraint';
+import { SchoolTerm } from '@app/shared/types';
+import { IsEnum, IsNumber } from 'class-validator';
+
+export class CreateResultDto {
+  @IsEnum(SchoolTerm)
+  term: SchoolTerm;
+
+  @IsNumber()
+  subject_total: number;
+
+  @IsNumber()
+  student_score: number;
+
+  @IsRegistered({ tableName: 'students', column: 'id' })
+  student_id: number;
+
+  @IsRegistered({ column: 'id', tableName: 'student_class' })
+  class_id: number;
+
+  @IsRegistered({ tableName: 'session', column: 'id' })
+  session_id: number;
+}
