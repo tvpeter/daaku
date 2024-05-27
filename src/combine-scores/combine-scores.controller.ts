20fc752b46e0680e49f51813106cbf01220a0efa
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-27 18:24:27 +0100

    add combine score controller

--- src/combine-scores/combine-scores.controller.ts
+++ src/combine-scores/combine-scores.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CombineScoresService } from './combine-scores.service';
 import { CreateCombineScoreDto } from './dto/create-combine-score.dto';
 import { UpdateCombineScoreDto } from './dto/update-combine-score.dto';
@@ -18,17 +27,20 @@ export class CombineScoresController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.combineScoresService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.combineScoresService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCombineScoreDto: UpdateCombineScoreDto) {
-    return this.combineScoresService.update(+id, updateCombineScoreDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCombineScoreDto: UpdateCombineScoreDto,
+  ) {
+    return this.combineScoresService.update(id, updateCombineScoreDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.combineScoresService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.combineScoresService.remove(id);
   }
 }
