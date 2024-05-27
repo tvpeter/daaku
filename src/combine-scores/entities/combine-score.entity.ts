d741a10db338e3c87f6c8d7622b3cdcdcbece2fe
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-27 15:42:01 +0100

    add combine scores entity structure

--- src/combine-scores/entities/combine-score.entity.ts
+++ src/combine-scores/entities/combine-score.entity.ts
@@ -1,1 +1,38 @@
-export class CombineScore {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('combine_scores')
+export class CombineScore {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'numeric', precision: 5, scale: 2, default: 0.0 })
+  term_1: number;
+
+  @Column({ type: 'numeric', precision: 5, scale: 2, default: 0.0 })
+  term_2: number;
+
+  @Column({ type: 'numeric', precision: 5, scale: 2, default: 0.0 })
+  term_3: number;
+
+  @Column({ type: 'numeric', precision: 4, scale: 2, default: 0.0 })
+  average: number;
+
+  @Column({ type: 'numeric', default: 0 })
+  position: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
