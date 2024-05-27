f87f5ed4d67b071932b20047b353e62d706f20d8
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-27 13:50:23 +0100

    add announcements entity

--- src/announcements/entities/announcement.entity.ts
+++ src/announcements/entities/announcement.entity.ts
@@ -1,1 +1,25 @@
-export class Announcement {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('announcements')
+export class Announcement {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  topic: string;
+
+  @Column({ type: 'text' })
+  body: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
