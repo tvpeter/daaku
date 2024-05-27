97c3aa5efa21c2339fed3f1952a7e313b8859a3a
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-27 18:19:23 +0100

    add combine score service

--- src/combine-scores/combine-scores.module.ts
+++ src/combine-scores/combine-scores.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CombineScoresService } from './combine-scores.service';
 import { CombineScoresController } from './combine-scores.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CombineScore } from './entities/combine-score.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CombineScore])],
   controllers: [CombineScoresController],
   providers: [CombineScoresService],
 })
--- src/combine-scores/combine-scores.service.ts
+++ src/combine-scores/combine-scores.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCombineScoreDto } from './dto/create-combine-score.dto';
 import { UpdateCombineScoreDto } from './dto/update-combine-score.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CombineScore } from './entities/combine-score.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CombineScoresService {
-  create(createCombineScoreDto: CreateCombineScoreDto) {
-    return 'This action adds a new combineScore';
+  constructor(
+    @InjectRepository(CombineScore)
+    private readonly combineScoreRepo: Repository<CombineScore>,
+  ) {}
+
+  async create(createCombineScoreDto: CreateCombineScoreDto) {
+    const combineScore = this.combineScoreRepo.create(createCombineScoreDto);
+
+    return await this.combineScoreRepo.save(combineScore);
   }
 
-  findAll() {
-    return `This action returns all combineScores`;
+  async findAll() {
+    return await this.combineScoreRepo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} combineScore`;
+  async findOne(id: number) {
+    const combineScore = await this.combineScoreRepo.findOne({ where: { id } });
+
+    if (!combineScore) {
+      throw new NotFoundException(
+        'Combine score for student for this session not found',
+      );
+    }
+
+    return combineScore;
   }
 
-  update(id: number, updateCombineScoreDto: UpdateCombineScoreDto) {
-    return `This action updates a #${id} combineScore`;
+  async update(id: number, updateCombineScoreDto: UpdateCombineScoreDto) {
+    const combineScore = await this.findOne(id);
+
+    return await this.combineScoreRepo.save({
+      ...combineScore,
+      ...updateCombineScoreDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} combineScore`;
+  async remove(id: number) {
+    const combineScore = await this.findOne(id);
+
+    return await this.combineScoreRepo.remove(combineScore);
   }
 }
