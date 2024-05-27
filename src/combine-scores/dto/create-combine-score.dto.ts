ea5792fadb1ef7742a4a4b8f07649720285d4a37
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-27 18:03:23 +0100

    add combine score dto

--- src/combine-scores/dto/create-combine-score.dto.ts
+++ src/combine-scores/dto/create-combine-score.dto.ts
@@ -1,1 +1,31 @@
-export class CreateCombineScoreDto {}
+import { IsRegistered } from '@app/shared/is-registered-constraint';
+import { IsNumber, IsOptional, IsPositive } from 'class-validator';
+
+export class CreateCombineScoreDto {
+  @IsRegistered({ tableName: 'students', column: 'id' })
+  student_id: number;
+
+  @IsRegistered({ tableName: 'student_class', column: 'id' })
+  class_id: number;
+
+  @IsRegistered({ tableName: 'session', column: 'id' })
+  session_id: number;
+
+  @IsRegistered({ tableName: 'subjects', column: 'id' })
+  subjeect_id: number;
+
+  @IsOptional()
+  @IsPositive()
+  @IsNumber()
+  term_1: number;
+
+  @IsOptional()
+  @IsPositive()
+  @IsNumber()
+  term_2: number;
+
+  @IsOptional()
+  @IsPositive()
+  @IsNumber()
+  term_3: number;
+}
