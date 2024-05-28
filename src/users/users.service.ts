1f1373152bd46c10fd6ebe139f39a3a8dca38e03
Vihiga Tyonum <withtvpeter@gmail.com>
2024-05-28 17:58:47 +0100

    change user status name and update selecting user details

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -16,7 +20,7 @@ export class UsersService {
   async create(createUserDto: CreateUserDto) {
     const hash = await bcrypt.hash(
       createUserDto.password,
-      this.configService.getOrThrow('SALT_ROUNDS'),
+      Number(this.configService.getOrThrow('SALT_ROUNDS')),
     );
     const user = this.userRepository.create({
       ...createUserDto,
@@ -54,9 +58,8 @@ export class UsersService {
 
   async findByUsername(username: string): Promise<User | null> {
     const user = await this.userRepository.findOne({ where: { username } });
-
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new UnauthorizedException('User not found');
     }
     return user;
   }
