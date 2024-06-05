9f1156fe221981832348d6de8809576eb6c780e3
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-05 21:39:44 +0100

    fix service tests for accounts, announcements and combine scores

--- src/announcements/announcements.service.spec.ts
+++ src/announcements/announcements.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnnouncementsService } from './announcements.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Announcement } from './entities/announcement.entity';
 
 describe('AnnouncementsService', () => {
   let service: AnnouncementsService;
 
+  const mockAnnouncementRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AnnouncementsService],
+      providers: [
+        AnnouncementsService,
+        {
+          provide: getRepositoryToken(Announcement),
+          useValue: mockAnnouncementRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<AnnouncementsService>(AnnouncementsService);
