feaf1de7441fd66c42de3295237cd78bf07ff684
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-05 22:00:15 +0100

    fix default tests for scores, sessions and student class

--- src/scores/scores.service.spec.ts
+++ src/scores/scores.service.spec.ts
@@ -1,13 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ScoresService } from './scores.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Score } from './entities/score.entity';
+import { ResultStatusService } from '@app/result-status/result-status.service';
 
 describe('ScoresService', () => {
   let service: ScoresService;
 
+  const mockScoresRepository = {};
+  const mockResultStatusService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ScoresService],
-    }).compile();
+      providers: [
+        ScoresService,
+        ResultStatusService,
+        {
+          provide: getRepositoryToken(Score),
+          useValue: mockScoresRepository,
+        },
+      ],
+    })
+      .overrideProvider(ResultStatusService)
+      .useValue(mockResultStatusService)
+      .compile();
 
     service = module.get<ScoresService>(ScoresService);
   });
--- src/studentclass/studentclass.service.spec.ts
+++ src/studentclass/studentclass.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentclassService } from './studentclass.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Studentclass } from './entities/studentclass.entity';
 
 describe('StudentclassService', () => {
   let service: StudentclassService;
 
+  const mockStudentClassRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StudentclassService],
+      providers: [
+        StudentclassService,
+        {
+          provide: getRepositoryToken(Studentclass),
+          useValue: mockStudentClassRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<StudentclassService>(StudentclassService);
