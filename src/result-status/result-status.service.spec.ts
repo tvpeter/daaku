3e24aa3c74f5276c346a217a95c07fc757ae7814
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-05 21:46:07 +0100

    fix default tests for result-status, results and score metadata

--- src/result-status/result-status.service.spec.ts
+++ src/result-status/result-status.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResultStatusService } from './result-status.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ResultStatus } from './entities/result-status.entity';
 
 describe('ResultStatusService', () => {
   let service: ResultStatusService;
 
+  const mockResultStatusRepository = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ResultStatusService],
+      providers: [
+        ResultStatusService,
+        {
+          provide: getRepositoryToken(ResultStatus),
+          useValue: mockResultStatusRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ResultStatusService>(ResultStatusService);
--- src/score-meta-data/score-meta-data.service.spec.ts
+++ src/score-meta-data/score-meta-data.service.spec.ts
@@ -1,17 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ScoreMetaDataService } from './score-meta-data.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ScoreMetaDatum } from './entities/score-meta-datum.entity';
 
 describe('ScoreMetaDataService', () => {
   let service: ScoreMetaDataService;
-  const mockScoreMetaDataService = {};
+  const mockScoreMetaDatumRepository = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ScoreMetaDataService],
-    })
-      .overrideProvider(ScoreMetaDataService)
-      .useValue(mockScoreMetaDataService)
-      .compile();
+      providers: [
+        ScoreMetaDataService,
+        {
+          provide: getRepositoryToken(ScoreMetaDatum),
+          useValue: mockScoreMetaDatumRepository,
+        },
+      ],
+    }).compile();
 
     service = module.get<ScoreMetaDataService>(ScoreMetaDataService);
   });
