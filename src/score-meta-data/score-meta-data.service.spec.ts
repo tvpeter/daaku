174fce59f1adbafe124005a220bf1168e70e2f57
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-05 20:50:58 +0100

    fix default tests for score metadata, scores and sessions module

--- src/score-meta-data/score-meta-data.service.spec.ts
+++ src/score-meta-data/score-meta-data.service.spec.ts
@@ -3,11 +3,15 @@ import { ScoreMetaDataService } from './score-meta-data.service';
 
 describe('ScoreMetaDataService', () => {
   let service: ScoreMetaDataService;
+  const mockScoreMetaDataService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ScoreMetaDataService],
-    }).compile();
+    })
+      .overrideProvider(ScoreMetaDataService)
+      .useValue(mockScoreMetaDataService)
+      .compile();
 
     service = module.get<ScoreMetaDataService>(ScoreMetaDataService);
   });
