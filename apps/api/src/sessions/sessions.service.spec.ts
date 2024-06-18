a78d5c9103631ca733f9baa9ead7c38373c367ba
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-18 17:10:28 +0100

    add test case for create session service

--- apps/api/src/sessions/sessions.service.spec.ts
+++ apps/api/src/sessions/sessions.service.spec.ts
@@ -2,11 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { SessionsService } from './sessions.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Session } from './entities/session.entity';
+import { mockSession, mockSessionDTO } from '@app/common/utils/mock-data';
 
 describe('SessionsService', () => {
   let service: SessionsService;
 
-  const mockSessionsRepository = {};
+  const mockSessionsRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,5 +31,19 @@ describe('SessionsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new session', async () => {
+    const session: Session = mockSession();
+
+    const sessionDto = mockSessionDTO(session);
+
+    mockSessionsRepository.create.mockResolvedValue(session);
+    mockSessionsRepository.save.mockResolvedValue(session);
+
+    const result = await service.create(sessionDto);
+
+    expect(mockSessionsRepository.create).toHaveBeenCalledWith(sessionDto);
+    expect(result).toEqual(session);
   });
 });
