da247657d7df6455fc5df34966740b4a1d286934
Vihiga Tyonum <withtvpeter@gmail.com>
2024-06-28 17:26:54 +0100

    add getting student result

--- apps/api/src/results/results.service.ts
+++ apps/api/src/results/results.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateResultDto } from './dto/create-result.dto';
 import { UpdateResultDto } from './dto/update-result.dto';
+import { SchoolTerm } from '@app/common/enums';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Result } from './entities/result.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ResultsService {
+  constructor(
+    @InjectRepository(Result)
+    private readonly resultRepository: Repository<Result>,
+  ) { }
+
   create(createResultDto: CreateResultDto) {
     return 'This action adds a new result';
   }
@@ -12,8 +21,23 @@ export class ResultsService {
     return `This action returns all results`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} result`;
+  async findOne(
+    student_id: number,
+    session_id: number,
+    term: SchoolTerm,
+  ): Promise<Result | null> {
+    const result = await this.resultRepository.findOne({
+      where: {
+        student_id,
+        session_id,
+        term,
+      },
+    });
+
+    if (!result)
+      throw new NotFoundException('Selected student result was not found');
+
+    return result;
   }
 
   update(id: number, updateResultDto: UpdateResultDto) {
