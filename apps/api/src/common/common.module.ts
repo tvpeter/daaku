f5c7102ec541661dac03aaba7b3c27d6a91bd9d7
Vihiga Tyonum <withtvpeter@gmail.com>
2024-08-08 15:30:32 +0100

    fix Entity manager import

--- apps/api/src/common/common.module.ts
+++ apps/api/src/common/common.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IsRegisteredConstraint } from './validators/is-registered-constraint';
 import { IsUniqueConstraint } from './validators/is-unique-constraint';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([])],
   providers: [IsRegisteredConstraint, IsUniqueConstraint],
   exports: [IsRegisteredConstraint, IsUniqueConstraint],
 })
