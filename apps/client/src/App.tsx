980aa1de63b4add58437b7ef5aeb8d2e0a1e0457
Vihiga Tyonum <withtvpeter@gmail.com>
2024-08-31 20:55:15 +0100

    add header top component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,65 +1,11 @@
 import SideNavBar from "./components/SideNavBar";
+import HeaderTop from "./components/HeaderTop";
 import "./App.css"
 
 function App() {
   return (
     <div id="wrapper" className="wrapper bg-ash">
-      <div className="navbar navbar-expand-md header-menu-one bg-light">
-        <div className="nav-bar-header-one">
-          <div className="header-logo">
-            <a href="index.html">
-              <img src="img/logo.png" alt="logo" />
-            </a>
-          </div>
-          <div className="toggle-button sidebar-toggle">
-            <button type="button" className="item-link">
-              <span className="btn-icon-wrap">
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
-            </button>
-          </div>
-        </div>
-        <div className="d-md-none mobile-nav-bar">
-          <button
-            className="navbar-toggler pulse-animation"
-            type="button"
-            data-toggle="collapse"
-            data-target="#mobile-navbar"
-            aria-expanded="false"
-          >
-            <i className="far fa-arrow-alt-circle-down"></i>
-          </button>
-          <button
-            type="button"
-            className="navbar-toggler sidebar-toggle-mobile"
-          >
-            <i className="fas fa-bars"></i>
-          </button>
-        </div>
-        <div
-          className="header-main-menu collapse navbar-collapse"
-          id="mobile-navbar"
-        >
-          <ul className="navbar-nav">
-            <li className="navbar-item header-search-bar">
-              <div className="input-group stylish-input-group">
-                <span className="input-group-addon">
-                  <button type="submit">
-                    <span className="flaticon-search" aria-hidden="true"></span>
-                  </button>
-                </span>
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Find Something . . ."
-                />
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <HeaderTop />
       <div className="dashboard-page-one">
         <div className="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color">
           <div className="mobile-sidebar-header d-md-none">
