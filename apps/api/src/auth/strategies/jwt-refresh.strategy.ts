033fde33212f1a3081105a7a3b94d8d6e76fac20
Vihiga Tyonum <withtvpeter@gmail.com>
2024-10-24 20:55:39 +0100

    add refresh token strategy

---
+++ apps/api/src/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,34 @@
+import { JwtPayload } from '@app/common/interfaces/jwt.interface';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(
+    configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies?.Refresh,
+      ]),
+      secretOrKey: configService.getOrThrow('JWT_REFRESH_TOKEN_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(request: Request, payload: JwtPayload) {
+    return this.authService.verifyUserRefreshToken(
+      request.cookies?.Refresh,
+      payload.userId,
+    );
+  }
+}
