fbe5ca75aedd33e294be22377b16068a86d8ee69
Vihiga Tyonum <withtvpeter@gmail.com>
2024-10-24 03:58:50 +0100

    extract token from cookie using cookie-parser

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -4,12 +4,16 @@ import { Injectable } from '@nestjs/common';
 import { JwtPayload } from '@app/common/interfaces/jwt.interface';
 import { AuthService } from '../auth.service';
 import { jwtConstants } from '../jwt-constants';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly authService: AuthService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      // jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => request.cookies?.Authentication,
+      ]),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
     });
