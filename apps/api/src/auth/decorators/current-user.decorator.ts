b4e91073be0c1edead7fdc686b1b9e7b61d61381
Vihiga Tyonum <withtvpeter@gmail.com>
2024-10-31 20:22:10 +0100

    rename user decorator to current user

--- apps/api/src/auth/decorators/current-user.decorator.ts
+++ apps/api/src/auth/decorators/current-user.decorator.ts
@@ -1,10 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator(
+export const CurrentUser = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     const user = request.user;
-
+    console.log('user in current user: ', user);
     return data ? user?.[data] : user;
   },
 );
