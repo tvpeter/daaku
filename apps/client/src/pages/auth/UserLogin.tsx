39cb088f5f1d2d1c88ad599cdafacb32c2b95d04
Vihiga Tyonum <withtvpeter@gmail.com>
2024-11-01 18:55:54 +0100

    add yup and form validation for login

--- apps/client/src/pages/auth/UserLogin.tsx
+++ apps/client/src/pages/auth/UserLogin.tsx
@@ -1,69 +1,85 @@
 import { useFormik } from "formik"
+import * as Yup from "yup"
+
+const validationSchema = Yup.object({
+  username: Yup.string().required("Username is required"),
+  password: Yup.string().required("Password is required"),
+})
 
 const UserLogin = () => {
-
   const formik = useFormik({
     initialValues: {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     },
-    onSubmit: values => {
-      console.log(values);
-    }
+    onSubmit: (values) => {
+      console.log(values)
+    },
+    validationSchema,
   })
-  
-  return (
-        <div className="login-page-wrap">
-          <div className="login-page-content">
-            <div className="login-box">
-              <div className="item-logo">
-                <img src="img/logo2.png" alt="logo" />
-              </div>
-              <form
-                action=""
-                className="login-form"
-                onSubmit={formik.handleSubmit}
-              >
-                <div className="form-group">
-                  <label>Username</label>
-                  <input
-                    type="text"
-                    placeholder="Enter usrename"
-                    className="form-control"
-                    name="username"
-                    value={formik.values.username}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Password</label>
-                  <input
-                    type="text"
-                    placeholder="Enter password"
-                    className="form-control"
-                    name="password"
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
 
-                </div>
-                <div className="form-group d-flex align-items-center justify-content-between">
-                
-                  <a href="#" className="forgot-btn">
-                    Forgot Password?
-                  </a>
-                </div>
-                <div className="form-group">
-                  <button type="submit" className="login-btn">
-                    Login
-                  </button>
-                </div>
-              </form>
-            </div>
+  return (
+    <div className="login-page-wrap">
+      <div className="login-page-content">
+        <div className="login-box">
+          <div className="item-logo">
+            <img src="img/logo2.png" alt="logo" />
           </div>
+          <form
+            action=""
+            className="login-form was-validated"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                placeholder="Enter usrename"
+                className="form-control is-invalid"
+                name="username"
+                id="username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                required
+              />
+              {formik.touched.username && formik.errors.username ? (
+                <div className="invalid-feedback">{formik.errors.username}</div>
+              ) : null}
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password" className="form-label">Password</label>
+              <input
+                type="text"
+                placeholder="Enter password"
+                className="form-control is-invalid"
+                name="password"
+                id="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                required
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <div className="invalid-feedback">{formik.errors.password}</div>
+              ) : null}
+            </div>
+
+            <div className="form-group d-flex align-items-center justify-content-between">
+              <a href="#" className="forgot-btn">
+                Forgot Password?
+              </a>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="login-btn">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
+    </div>
   )
 }
 
