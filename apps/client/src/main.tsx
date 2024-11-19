dc5e482ad80c2b74835f738f2c96b7d51ff0f85d
Vihiga Tyonum <withtvpeter@gmail.com>
2024-11-19 17:55:42 +0100

    refactor side nav page

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import "bootstrap/dist/css/bootstrap.css"
 import { RouterProvider } from "react-router-dom"
 import router from "./routes/router.tsx"
 import AuthProvider from "react-auth-kit"
 import store from "./pages/auth/AuthStore.ts"
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
