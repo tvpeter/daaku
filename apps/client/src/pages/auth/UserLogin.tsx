1dd482d4da04a1c82f3297b7a37fc21bd38dd4da
Vihiga Tyonum <withtvpeter@gmail.com>
2024-11-20 09:50:16 +0100

    redesign the login form

--- apps/client/src/pages/auth/UserLogin.tsx
+++ apps/client/src/pages/auth/UserLogin.tsx
@@ -15,6 +15,7 @@ const UserLogin = () => {
   const [error, setError] = useState("")
   const signIn = useSignIn()
   const navigate = useNavigate()
+  const [showPassword, setShowPassword] = useState(false)
 
   const onSubmit = async (values: { username: string; password: string }) => {
     setError("")
@@ -51,62 +52,94 @@ const UserLogin = () => {
   })
 
   return (
-    <div className="login-page-wrap">
-      <div className="login-page-content">
-        <div className="login-box">
-          <div className="item-logo">
-            {/* <img src="img/logo2.png" alt="logo" /> */}
-          </div>
-          {error && <div className="text-danger mb-3">{error}</div>}
-          <form
-            action=""
-            className="login-form was-validated"
-            onSubmit={formik.handleSubmit}
-          >
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                placeholder="Enter usrename"
-                className="form-control is-invalid"
-                id="username"
-                {...formik.getFieldProps("username")}
-                required
-              />
+    <div className="row align-items-center justify-content-center vh-100">
+      <div className="col-11 col-sm-8 col-md-6 col-lg-5 col-xl-4 col-xxl-3 py-6">
+        <h1 className="mb-2 text-center">Sign In</h1>
+
+        <p className="text-secondary text-center">
+          Enter your username and password to login
+        </p>
+        {error && <div className="text-bg-danger-soft p-3">{error}</div>}
+        <form action="" onSubmit={formik.handleSubmit} className="was-validated">
+          <div className="row">
+            <div className="col-12">
+              <div className="mb-4">
+                <label className="form-label">Username</label>
+
+                <input
+                  type="text"
+                  className="form-control is-invalid"
+                  placeholder="Your username"
+                  id="username"
+                  {...formik.getFieldProps("username")}
+                  required
+                />
+              </div>
               {formik.touched.username && formik.errors.username ? (
                 <div className="invalid-feedback">{formik.errors.username}</div>
               ) : null}
             </div>
 
-            <div className="form-group">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input
-                type="text"
-                placeholder="Enter password"
-                className="form-control is-invalid"
-                id="password"
-                {...formik.getFieldProps("password")}
-                required
-              />
+            <div className="col-12">
+              <div className="mb-4">
+                <div className="row">
+                  <div className="col">
+                    <label className="form-label">Password</label>
+                  </div>
+
+                  <div className="col-auto">
+                    <a
+                      href=""
+                      className="form-text small text-muted link-primary"
+                    >
+                      Forgot password
+                    </a>
+                  </div>
+                </div>
+
+                <div className="input-group input-group-merge">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="form-control is-invalid"
+                    autoComplete="off"
+                    data-toggle-password-input
+                    id="password"
+                    placeholder="Your password"
+                    {...formik.getFieldProps("password")}
+                    required
+                  />
+
+                  <button
+                    type="button"
+                    className="input-group-text px-4 text-secondary link-primary"
+                    data-toggle-password
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                  </button>
+                </div>
+              </div>
               {formik.touched.password && formik.errors.password ? (
                 <div className="invalid-feedback">{formik.errors.password}</div>
               ) : null}
             </div>
+          </div>
 
-            <div className="form-group d-flex align-items-center justify-content-between">
-              <a href="#" className="forgot-btn">
-                Forgot Password?
-              </a>
-            </div>
-            <div className="form-group">
-              <button type="submit" className="login-btn">
-                Login
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" id="remember" />
+
+            <label className="form-check-label" htmlFor="remember">
+              Remember me
+            </label>
+          </div>
+
+          <div className="row align-items-center text-center">
+            <div className="col-12">
+              <button type="submit" className="btn w-100 btn-primary mt-6 mb-2">
+                Sign in
               </button>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   )
