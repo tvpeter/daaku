9021e36bdfe6b5836596d2c58e821944a7f60004
Vihiga Tyonum <withtvpeter@gmail.com>
2024-11-25 21:56:28 +0100

    fix classes page

--- apps/client/src/pages/studentclass/StudentsClass.tsx
+++ apps/client/src/pages/studentclass/StudentsClass.tsx
@@ -1,11 +1,4 @@
-import {
-  faTimes,
-  faCogs,
-  faRedoAlt,
-  faTrash,
-  faEdit,
-  faExclamationTriangle,
-} from "@fortawesome/free-solid-svg-icons"
+import { faTrash, faEdit, faPlus, faExclamationTriangle } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
 import studentClassService, {
@@ -16,7 +9,6 @@ import { AxiosError } from "axios"
 const StudentsClass = () => {
   const [studentclass, setStudentclass] = useState<StudentClass[]>([])
   const [error, setError] = useState("")
-  const [isLoading, setLoading] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [deleteId, setDeleteId] = useState<number | null>(null)
 
@@ -25,7 +17,6 @@ const StudentsClass = () => {
       result: StudentClass[]
     }>()
 
-    setLoading(true)
     request
       .then((response) => {
         setStudentclass(response.data.result)
@@ -33,7 +24,6 @@ const StudentsClass = () => {
       .catch((error) => {
         if (error && error instanceof AxiosError) {
           setError(error.response?.data.message)
-          setLoading(false)
         } else if (error && error instanceof Error) setError(error.message)
       })
     return () => cancel()
@@ -56,12 +46,11 @@ const StudentsClass = () => {
         .then(() => {
           setStudentclass((studentclass) =>
             studentclass.filter((stclass) => stclass.id !== deleteId)
-          );
+          )
         })
         .catch((error) => {
           if (error && error instanceof AxiosError) {
             setError(error.response?.data.message)
-            setLoading(false)
           } else if (error && error instanceof Error) setError(error.message)
         })
         .finally(() => {
@@ -71,100 +60,119 @@ const StudentsClass = () => {
   }
 
   return (
-    <div className="dashboard-content-one">
-      <div className="breadcrumbs-area">
-        <h3>Classes</h3>
-        <ul>
-          <li>
-            <a href="index.html">Home</a>
-          </li>
-          <li>All Classes</li>
-        </ul>
+    <div className="container-fluid">
+      <div className="d-flex align-items-baseline justify-content-between">
+        <h1 className="h2">All Classes</h1>
+
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="#">Pages</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Classes
+            </li>
+          </ol>
+        </nav>
       </div>
-      <div className="card height-auto">
-        <div className="card-body">
-          <div className="heading-layout1">
-            <div className="item-title"></div>
-            <div className="dropdown">
-              <div className="dropdown-menu dropdown-menu-right">
-                <a className="dropdown-item" href="#">
+
+      {error && (
+        <div
+          className="alert d-flex align-items-center mb-6 text-bg-danger-soft"
+          role="alert"
+        >
+          {error}
+        </div>
+      )}
+
+      <div className="row mt-9">
+        <div className="col d-flex">
+          <div className="card border-0 flex-fill w-100">
+            <div className="card-header border-0">
+              <div className="d-flex flex-column flex-md-row align-items-md-center justify-content-between">
+                <h2 className="card-header-title h4 text-uppercase ">
+                  Classes
+                </h2>
+
+                <button
+                  type="button"
+                  className="btn btn-primary ms-md-4"
+                  data-bs-toggle="modal"
+                  data-bs-target="#createKeyModal"
+                >
                   <FontAwesomeIcon
-                    icon={faTimes}
-                    className="text-orange-red"
-                    title="Edit"
-                  ></FontAwesomeIcon>
-                </a>
-                <a className="dropdown-item" href="#">
-                  <FontAwesomeIcon
-                    icon={faCogs}
-                    title="Print"
-                  ></FontAwesomeIcon>
-                </a>
-                <a className="dropdown-item" href="#">
-                  <FontAwesomeIcon
-                    icon={faRedoAlt}
-                    className="text-orange-peel"
-                    title="Download"
-                  ></FontAwesomeIcon>
-                </a>
+                    icon={faPlus}
+                    height={1}
+                    width={14}
+                    className="me-1"
+                  />
+                  Add Class
+                </button>
               </div>
             </div>
-          </div>
 
-          {error && <div className="text-danger mb-3">{error}</div>}
-          {isLoading && (
-            <div className="spinner-border text-success" role="status">
-              <span className="sr-only">Loading...</span>
-            </div>
-          )}
-          <div className="table-responsive">
-            <table className="table display data-table text-nowrap">
-              <thead>
-                <tr>
-                  <th>SN</th>
-                  <th>Class</th>
-                  <th>Created</th>
-                  <th>Last Updated</th>
-                  <th>Update</th>
-                  <th>Delete</th>
-                </tr>
-              </thead>
-              <tbody>
-                {studentclass.map((classDetails) => (
-                  <tr key={classDetails.id}>
-                    <td>{classDetails.id}</td>
-                    <td>{classDetails.name} </td>
-                    <td>
-                      {new Date(classDetails.created_at).toLocaleDateString(
-                        "en-GB"
-                      )}
-                    </td>
-                    <td>
-                      {new Date(classDetails.updated_at).toLocaleDateString(
-                        "en-GB"
-                      )}
-                    </td>
-                    <td>
-                      <FontAwesomeIcon
-                        icon={faEdit}
-                        title="Edit"
-                      ></FontAwesomeIcon>
-                    </td>
-                    <td>
-                        <button onClick={() => openModal(classDetails.id)} className="border-0 bg-transparent">
-                      <FontAwesomeIcon
-                        icon={faTrash}
-                        className="text-orange-red stretched-link"
-                        title="Delete"
-                      />
-                      </button>
-                    </td>
+            <div className="table-responsive">
+              <table className="table align-middle table-hover table-nowrap mb-0">
+                <thead className="thead-light">
+                  <tr>
+                    <th>S/N</th>
+                    <th>Name</th>
+                    <th>Date Created</th>
+                    <th>Date Updated</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
 
-            {isModalOpen && (
+                <tbody className="list">
+                  {studentclass.map((classDetails) => (
+                    <tr key={classDetails.id}>
+                      <td>{classDetails.id}</td>
+                      <td>{classDetails.name}</td>
+                      <td>
+                        {new Date(classDetails.created_at).toLocaleDateString(
+                          "en-GB"
+                        )}
+                      </td>
+                      <td className="status text-capitalize">
+                        {new Date(classDetails.updated_at).toLocaleDateString(
+                          "en-GB"
+                        )}
+                      </td>
+                      <td>
+                        {" "}
+                        <FontAwesomeIcon
+                          icon={faEdit}
+                          title="Edit"
+                        ></FontAwesomeIcon>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => openModal(classDetails.id)}
+                          className="border-0 bg-transparent"
+                        >
+                          <FontAwesomeIcon
+                            icon={faTrash}
+                            className="text-orange-red stretched-link"
+                            title="Delete"
+                          />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="card-footer">
+              <ul className="pagination justify-content-end list-pagination mb-0"></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal */}
+      {isModalOpen && (
             <div
             className="modal"
             id="confirmation-modal"
@@ -203,9 +211,6 @@ const StudentsClass = () => {
             </div>
           </div>
             )}
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
