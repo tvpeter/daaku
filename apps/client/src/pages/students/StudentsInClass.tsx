e4a46dadc788df2baaedbd7ebc0f4edd34b5655f
Vihiga Tyonum <withtvpeter@gmail.com>
2024-11-28 21:16:02 +0100

    add getting students in a class

---
+++ apps/client/src/pages/students/StudentsInClass.tsx
@@ -0,0 +1,333 @@
+import React from "react"
+import { useEffect, useState } from "react"
+import studentService, { Student } from "../../services/studentService"
+import { AxiosError } from "axios"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom"
+import sessionService, {
+  SchoolSession,
+  SessionStatus,
+} from "../../services/sessionService"
+import studentClassService, { StudentClass } from "../../services/studentClassService"
+
+const StudentsInClass = () => {
+  const [error, setError] = useState("")
+  const [students, setStudents] = useState<Student[]>([])
+  const [isLoading, setLoading] = useState(false)
+  const [sessions, setSessions] = useState<SchoolSession[]>([])
+  const [selectedSessionId, setSelectedSessionId] = useState<number | null>(
+    null
+  )
+  const [selectedSessionName, setSelectedSessionName] = useState<string | null>(
+    null
+  )
+  const [studentClass, setStudentClass] = useState<StudentClass[]>([]);
+
+  const getSessions = () => {
+    const { request, cancel } = sessionService.getAll<{
+      result: SchoolSession[]
+    }>()
+
+    request
+      .then((response) => {
+        const fetchedSessions = response.data.result
+        const openSessions = fetchedSessions
+          ? fetchedSessions.filter(
+              (session) => session.status === SessionStatus.OPEN
+            )
+          : []
+        setSessions(openSessions)
+      })
+      .catch((error) => {
+        if (error instanceof AxiosError) {
+          setError(error.response?.data.message)
+        } else if (error && error instanceof Error) setError(error.message)
+      })
+
+    return () => cancel()
+  }
+
+
+  const loadStudentClasses = () => {
+    const { request, cancel } = studentClassService.getAll<{
+      result: StudentClass[]
+    }>()
+
+    request
+      .then((response) => {
+        setStudentClass(response.data.result)
+      })
+      .catch((error) => {
+        if (error && error instanceof AxiosError) {
+          setError(error.response?.data.message)
+        } else if (error && error instanceof Error) setError(error.message)
+      })
+    return () => cancel()
+  }
+
+
+
+
+  const getStudents = (session_id?: number) => {
+    let requestBody
+    if (session_id) {
+      requestBody = studentService.getWithParams<
+        { session_id: number },
+        { result: Student[] }
+      >({ session_id })
+    } else {
+      requestBody = studentService.getAll<{ result: Student[] }>()
+    }
+
+    setLoading(true)
+    requestBody.request
+      .then((response) => {
+        setStudents(response.data.result)
+        setLoading(false)
+      })
+      .catch((error) => {
+        if (error instanceof AxiosError) {
+          setError(error.response?.data.message)
+        } else if (error && error instanceof Error) setError(error.message)
+      })
+    setLoading(false)
+
+    return () => requestBody.cancel()
+  }
+
+  useEffect(() => {
+    getSessions()
+    loadStudentClasses()
+  }, [])
+
+  const handleSessionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const sessionId = event.target.value
+    setSelectedSessionId(Number(sessionId))
+    setSelectedSessionName(
+      event.target.options[event.target.selectedIndex].text
+    )
+  }
+
+  useEffect(() => {
+    selectedSessionId ? getStudents(selectedSessionId) : getStudents()
+  }, [selectedSessionId])
+
+ 
+
+
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="d-flex align-items-baseline justify-content-between">
+          <h1 className="h2">Students</h1>
+
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item"></li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Students In A Class
+              </li>
+            </ol>
+          </nav>
+        </div>
+        {error && (
+          <div
+            className="alert d-flex align-items-center mb-6 text-bg-danger-soft"
+            role="alert"
+          >
+            {error}
+          </div>
+        )}
+
+        {isLoading && (
+          <div className="spinner-border text-success" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        )}
+
+        <div className="row">
+          <div className="col d-flex mt-9">
+            <div className="card border-0 flex-fill w-100">
+              <div className="card-header border-0">
+                <div className="d-flex flex-column flex-md-row align-items-md-center justify-content-between">
+                  <h2 className="card-header-title h4 text-uppercase">
+                    {selectedSessionName
+                      ? `All Students for ${selectedSessionName} Session`
+                      : "All Students"}
+                  </h2>
+
+                  <form className="d-flex align-items-center gap-4">
+                    <select
+                      className="form-select"
+                      id="session_id"
+                    >
+                      <option value="" label="Select session"></option>
+                      {sessions.map((session) => (
+                        <option value={session.id} key={session.id}>
+                          {session.name}
+                        </option>
+                      ))}
+                    </select>
+                    <select
+                      className="form-select"
+                      id="class_id"
+                    >
+                      <option value="" label="Select class"></option>
+                      {studentClass.map((classDetails) => (
+                        <option value={classDetails.id} key={classDetails.id}>
+                          {classDetails.name}
+                        </option>
+                      ))}
+                    </select>
+                    <button type="submit" className="btn btn-outline-primary btn-sm">
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+
+              <div className="table-responsive">
+                <table className="table align-middle table-hover table-nowrap mb-0">
+                  <thead className="thead-light">
+                    <tr>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="index"
+                        >
+                          S/N
+                        </a>
+                      </th>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="name"
+                        >
+                          Name
+                        </a>
+                      </th>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="admission_number"
+                        >
+                          Adm No.
+                        </a>
+                      </th>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="gender"
+                        >
+                          Gender
+                        </a>
+                      </th>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="session.name"
+                        >
+                          Session
+                        </a>
+                      </th>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="class.name"
+                        >
+                          Class
+                        </a>
+                      </th>
+                      <th>
+                        <a
+                          href="#"
+                          className="text-muted list-sort"
+                          data-sort="created_at"
+                        >
+                          Reg. Date
+                        </a>
+                      </th>
+                      <th>
+                        <a href="#" className="text-muted list-sort">
+                          Action
+                        </a>
+                      </th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="list">
+                    {students.map((student, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td className="name">
+                          <Link
+                            to={`/app/students/details/${student.id}`}
+                            className="text-gray-600"
+                          >
+                            {student.name}
+                          </Link>
+                        </td>
+                        <td>{student.admission_number}</td>
+                        <td className="status text-capitalize">
+                          {student.gender}
+                        </td>
+                        <td className="created">{student.session.name}</td>
+                        <td className="created">{student.class?.name}</td>
+                        <td className="created">
+                          {new Date(student.created_at).toLocaleDateString(
+                            "en-GB"
+                          )}
+                        </td>
+                        <td>
+                          <div className="dropdown float-end">
+                            <a
+                              href="#"
+                              className="dropdown-toggle no-arrow d-flex text-secondary"
+                              role="button"
+                              data-bs-toggle="dropdown"
+                              aria-haspopup="true"
+                              aria-expanded="false"
+                            >
+                              <FontAwesomeIcon
+                                icon={faEllipsisH}
+                                height={14}
+                                width={14}
+                              />
+                            </a>
+                            <ul className="dropdown-menu">
+                              <li>
+                                <a className="dropdown-item" href="#">
+                                  Edit
+                                </a>
+                              </li>
+                             
+                            </ul>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+              <div className="card-footer">
+                <ul className="pagination justify-content-end list-pagination mb-0"></ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+     
+    </>
+  )
+}
+
+export default StudentsInClass
