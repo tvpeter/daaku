06bc52d58fe5ded90b15698eaeba943daf08d472
Vihiga Tyonum <withtvpeter@gmail.com>
2024-12-14 23:20:15 +0100

    add student subjects query dto

---
+++ apps/api/src/student-subject-registration/dto/find-student-subject-reg-query.dto.ts
@@ -0,0 +1,23 @@
+import { IsRegistered } from '@app/common/validators/is-registered-constraint';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional } from 'class-validator';
+
+export class FindStudentSubjectRegQueryDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @IsRegistered({ tableName: 'student_class', column: 'id' })
+  class_id?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @IsRegistered({ tableName: 'session', column: 'id' })
+  session_id?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @IsRegistered({ tableName: 'subjects', column: 'id' })
+  subject_id?: string;
+}
